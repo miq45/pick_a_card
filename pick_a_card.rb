3dcf5e122ef1da928fdc9f1f10a411811082bd37
Mikhail <mbarykin970@gmail.com>
2022-08-02 15:02:46 +0500

    change suits to unicode

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
-suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample.encode('utf-8')}"
